fdb5e664a56b864eb12aff502aacebbb5ebbcdeb
0x46641766F7572 <96478305+favourolaoye@users.noreply.github.com>
2025-06-10 16:27:03 +0100

    init

--- maps-funcs/func.go
+++ maps-funcs/func.go
@@ -2,28 +2,41 @@
 
 package main
 
-import (
-	"fmt"
-	"math/rand"
-	"time"
-)
+import "fmt"
+
+// import (
+// 	"fmt"
+// 	"math/rand"
+// 	"time"
+// )
 
 // func myfunc(param type, param type) return_type {
 // code block
 // must return something
 // }
 
-func randomizer(elements ...int) int{
+// func randomizer(elements ...int) int{
 
-	if len(elements) == 0 {
-		return -1 
+// 	if len(elements) == 0 {
+// 		return -1
+// 	}
+
+// 	rgen := rand.New(rand.NewSource(time.Now().UnixNano()))
+// 	return elements[rgen.Intn(len(elements))]
+
+// }
+// func main() {
+// 	randomValue := randomizer(1, 2, 3, 4, 5, 6, 7, 8, 9)
+// 	fmt.Println("Randomly selected value:", randomValue)
+// }
+
+func main(){
+    myobject := map[string]int {
+		"key": 20,
+		"key2": 30,
 	}
-
-	r := rand.New(rand.NewSource(time.Now().UnixNano())) 
-	return elements[r.Intn(len(elements))]
-
-}
-func main() {
-	randomValue := randomizer(1, 2, 3, 4, 5, 6, 7, 8, 9)
-	fmt.Println("Randomly selected value:", randomValue)
+	fmt.Println(myobject)
+	for key, value := range myobject{
+		fmt.Printf("%v -> %v \n", key, value)
+	}
 }
